6959ab0e584b259339f820391c0ddbe5079d5775
Angelos <angelos.makris1907@gmail.com>
2026-05-31 17:55:03 +0100

    Created Header Component

---
+++ angelos-react/src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>Angelos</h1>
+                <p>Your voice planner</p>
+            </header>
+        </>
+    );
+}
+
+export default Header;
