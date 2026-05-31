5330e1b6224fc4805f0ea581d3a5dab9023d6f3a
Angelos <angelos.makris1907@gmail.com>
2026-05-31 18:57:26 +0100

    Created TaskInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import NextTask from "./components/NextTask";
+import TaskInput from "./components/TaskInput";
 
 function App() {
   return (
     <main className="app">
       <Header />
       <NextTask />
+      <TaskInput />
     </main>
   );
 }
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,16 @@
+function TaskInput() {
+  return (
+    <section>
+      <h2>Add Task</h2>
+
+      <input
+        type="text"
+        placeholder="Enter a task..."
+      />
+
+      <button>Add</button>
+    </section>
+  );
+}
+
+export default TaskInput;
