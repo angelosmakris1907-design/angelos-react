c5d1eb208087fde0bc1701c937f2b97bba2d21d0
Angelos <angelos.makris1907@gmail.com>
2026-06-02 16:05:15 +0100

    Made notes section collapsible

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,32 +1,35 @@
 function NotesList({ notes, onDeleteNote, onEditNote }) {
     return (
-        <section>
-            <h2>Notes</h2>
+        <details>
+            <summary>Notes</summary>
+                <section>
+                    <h2>Notes</h2>
 
-            {notes.length === 0 ? (
-                <p>No notes yet.</p>
-            ) : (
-                notes.map((note) => (
-                    <p key={note.id}>
-                        {note.text}
-                        <button onClick={() => onDeleteNote(note.id)}>
-                            Delete
-                        </button>
-                        <button
-                            onClick={() => {
-                                const newText = prompt("Edit note:", note.text);
+                    {notes.length === 0 ? (
+                        <p>No notes yet.</p>
+                    ) : (
+                        notes.map((note) => (
+                            <p key={note.id}>
+                                {note.text}
+                                <button onClick={() => onDeleteNote(note.id)}>
+                                    Delete
+                                </button>
+                                <button
+                                    onClick={() => {
+                                        const newText = prompt("Edit note:", note.text);
 
-                                if (newText && newText.trim() !== "") {
-                                    onEditNote(note.id, newText.trim());
-                                }
-                            }}
-                        >
-                            Edit
-                        </button>
-                    </p>
-                ))
-            )}
-        </section>
+                                        if (newText && newText.trim() !== "") {
+                                            onEditNote(note.id, newText.trim());
+                                        }
+                                    }}
+                                >
+                                    Edit
+                                </button>
+                            </p>
+                        ))
+                    )}
+                </section>
+        </details>
     );
 }
 
